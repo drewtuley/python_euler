4034f837a69b8f1db43431d2c22b24fab15beb2e
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-07-20 14:53:49 +0100

    euler 14 - Longest Collatz sequence

---
+++ PythonEuler/src/euler14.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="andrew.tuley"
+__date__ ="$20-Jul-2015 14:47:42$"
+
+if __name__ == "__main__":
+    """
+    The following iterative sequence is defined for the set of positive integers:
+
+    n → n/2 (n is even)
+    n → 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+
+    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+    Which starting number, under one million, produces the longest chain?
+
+    NOTE: Once the chain starts the terms are allowed to go above one million
+    """
+
+    start=(1e6)-1
+    val = start
+    chain=1
+    while val > 1:
+        #print chain, val
+        if val % 2 == 0:
+            val /= 2
+        else:
+            val = 1+(val*3)
+        chain += 1
+    
+    print val, chain
